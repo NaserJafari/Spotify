07766f0d5a58e037e7af6abe4d020e087341efb0
Naser Jafari <naserjafari5002@gmail.com>
2023-05-12 09:21:07 +0200

    Add project files.

---
+++ Spotify/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Spotify
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to Spotify!");
+        }
+    }
+}
