fc46134a2ce46688fde1937ddb9c5482dfae9f44
Naser Jafari <naserjafari5002@gmail.com>
2023-06-01 15:23:23 +0200

    classes toegevoegd en start spotify

--- Spotify/Program.cs
+++ Spotify/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, welcome to Spotify!");
+            Song song = new("Titel song", "Genre song", 123, 2001);
+            song.PlaySong();
+            song.DisplaySong();
         }
     }
 }
---
+++ Spotify/Song.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify
+{
+    internal class Song
+    {
+        public string title;
+        public string genre;
+        public int seconds;
+        public int year;
+        //public Artist artist;
+        
+        public Song(string title, string genre, int seconds, int year)
+        {
+            this.title = title;
+            this.genre = genre;
+            this.seconds = seconds;
+            this.year = year;
+        }
+
+        public void PlaySong()
+        {
+            Console.WriteLine("Playing " + title);
+        }
+
+        public void DisplaySong()
+        {
+            Console.WriteLine("Playing: " + title + ", Genre: " + genre + ", Seconds: " + seconds + ", Year: " + year);
+        }
+    }
+}
